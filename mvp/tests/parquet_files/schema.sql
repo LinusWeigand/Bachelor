59e4c3ae8a3f2f968cfca7a9227fc77a88e9d154
Linus Weigand <linusweigand@protonmailcom>
2024-10-22 17:28:30 +0200

    add tests

--- mvp/tests/parquet_files/schema.sql
+++ mvp/tests/parquet_files/schema.sql
@@ -5,5 +5,7 @@ INSERT INTO people VALUES ('Alice', 34), ('Bob', 45), ('Charlie', 23);
 COPY people TO 'output.parquet' (FORMAT 'parquet');
 
 SELECT * FROM 'output.parquet';
+SELECT * FROM 'download.parquet';
+
 
 SELECT * FROM 'test_file2.parquet';
--- mvp/tests/tests.sh
+++ mvp/tests/tests.sh
@@ -1,9 +1,20 @@
 # Health Check
+export IP_ADDRESS=18.159.254.5
+
 curl -X GET http://localhost:8000/api/healthchecker
+curl -X GET http://$IP_ADDRESS:8000/api/healthchecker
 
 # PUT Parquet File
 curl -X PUT http://localhost:8000/parquet/test_file.parquet \
   -F "file=@/Users/linusweigand/Universität/7.Semester/Bachelor/mvp/tests/parquet_files/output.parquet"
 
+curl -X PUT http://$IP_ADDRESS:8000/parquet/test_file.parquet \
+  -F "file=@/Users/linusweigand/Universität/7.Semester/Bachelor/mvp/tests/parquet_files/output.parquet"
+
 # GET Parquet File
+URL="http://$IP_ADDRESS:8000/parquet/test_file.parquet"
+OUTPUT_FILE="download.parquet"
+status_code=$(curl -s -o "$OUTPUT_FILE" -w "%{http_code}" "$URL")
+echo $status_code
 curl -X GET http://localhost:8000/parquet/test_file.parquet --output download.parquet
+curl -X GET http://$IP_ADDRESS:8000/parquet/test_file.parquet --output download.parquet
