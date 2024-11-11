dafe031d0112dac31e73db94594351a5b98aec4e
Linus Weigand <linusweigand@protonmailcom>
2024-11-11 19:33:26 +0100

    change server port to 80

--- mvp/src/server/mod.rs
+++ mvp/src/server/mod.rs
@@ -8,6 +8,6 @@ const PARQUET_FOLDER: &str = "./parquet_files/";
 #[tokio::main]
 async fn main() {
     let app = route::create_router().layer(DefaultBodyLimit::max(40 * 1024 * 1024));
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:80").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
