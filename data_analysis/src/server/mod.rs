2b7572fe9ac9eb16248f28b38f6c74a4c1d9b6e8
Linus Weigand <linusweigand@protonmailcom>
2024-11-19 11:12:59 +0100

    add server benchmark plotting

---
+++ data_analysis/src/server/mod.rs
@@ -0,0 +1,162 @@
+pub mod server_etl;
+pub mod server_plot;
+
+use axum::{
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
+use server_plot::{get_instance_metric_data};
+use server_plot::MetricDataJson;
+use tokio::main;
+use tower_http::compression::CompressionLayer;
+
+const DURATION: f64 = 359.;
+
+#[main]
+async fn main() {
+    println!("Runinng: http://127.0.0.1:5555");
+
+    let compression_layer = CompressionLayer::new().br(true).gzip(true).deflate(true);
+    let app = Router::new()
+        .route("/", get(index))
+        .layer(compression_layer);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:5555").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+fn generate_chart_set_html(
+    title: &str,
+    chart_prefix: &str,
+    data: &MetricDataJson,
+) -> String {
+    format!(
+        r#"
+        <h2>{}</h2>
+        <div class="grid-container">
+            <div class="chart-item">
+                <h2>vCPU </h2>
+                <div id="{chart_prefix}-cpu" class="chart-container"></div>
+            </div>
+            <div class="chart-item">
+                <h2>RAM</h2>
+                <div id="{chart_prefix}-ram" class="chart-container"></div>
+            </div>
+            <div class="chart-item">
+                <h2>Bytes Send</h2>
+                <div id="{chart_prefix}-send" class="chart-container"></div>
+            </div>
+            <div class="chart-item">
+                <h2>Bytes Received</h2>
+                <div id="{chart_prefix}-received" class="chart-container"></div>
+            </div>
+           
+        </div>
+        <script>
+            const chart{chart_prefix}_cpu = initChart('{chart_prefix}-cpu', {cpu});
+            const chart{chart_prefix}_ram = initChart('{chart_prefix}-ram', {ram});
+            const chart{chart_prefix}_send = initChart('{chart_prefix}-send', {send});
+            const chart{chart_prefix}_received = initChart('{chart_prefix}-received', {received});
+
+            window.addEventListener('resize', function() {{
+                chart{chart_prefix}_cpu.resize();
+                chart{chart_prefix}_ram.resize();
+                chart{chart_prefix}_send.resize();
+                chart{chart_prefix}_received.resize();
+            }});
+        </script>
+        "#,
+        title,
+        chart_prefix = chart_prefix,
+        cpu = data.cpu,
+        ram = data.ram,
+        send = data.send,
+        received = data.received,
+    )
+}
+
+async fn index() -> impl IntoResponse {
+    let v0 = get_instance_metric_data(0);
+
+    let chart_sets_html = vec![
+        generate_chart_set_html(
+            "v0: Receive Package into RAM -> write into file",
+            "chart1",
+            &v0,
+        ),
+    ]
+    .join("\n");
+
+    let combined_html = format!(
+        r#"
+        <html>
+        <head>
+            <meta charset="UTF-8">
+            <title>Combined Charts</title>
+            <script src="https://cdn.jsdelivr.net/npm/echarts/dist/echarts.min.js"></script>
+            <style>
+                html, body {{
+                    margin: 0;
+                    padding: 0;
+                    height: 100%;
+                }}
+                .grid-container {{
+                    display: grid;
+                    grid-template-columns: 1fr 1fr;
+                    grid-template-rows: 1fr 1fr;
+                    gap: 10px;
+                    width: 100%;
+                    height: 100%;
+                }}
+                .chart-item {{
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    width: 100%;
+                    height: 100%;
+                }}
+                .chart-container {{
+                    height: 100%;
+                    width: 100%;
+                }}
+                h1 {{
+                    text-align: center;
+                    font-family: Arial, sans-serif;
+                }}
+                h2 {{
+                    margin: 10px 0; 
+                    font-family: Arial, sans-serif;
+                    text-align: center;
+                }}
+            </style>
+        </head>
+        <body>
+                       <script>
+                const initChart = (containerId, options) => {{
+                    const chart = echarts.init(document.getElementById(containerId), null, {{
+                        renderer: 'canvas',
+                        useDirtyRect: true
+                    }});
+                    if (options === undefined) {{
+
+                        console.log('options undefined for chart: ', containerID);
+                    }}
+                    options.series[0].large = true;
+                    options.series[0].largeThreshold = 1000;
+                    options.series[0].progressive = 1000;
+                    options.series[0].progressiveThreshold = 1000;
+                    chart.setOption(options);
+                    return chart;
+                }};
+            </script>
+            {chart_sets_html}
+
+
+        </body>
+        </html>
+        "#,
+        chart_sets_html = chart_sets_html
+    );
+
+    Html(combined_html).into_response()
+}
---
+++ data_analysis/src/server/server_plot.rs
@@ -0,0 +1,160 @@
+use charming::datatype::{CompositeValue, NumericValue};
+use charming::element::AxisType;
+use charming::element::{AreaStyle, ItemStyle, Symbol, Trigger};
+use charming::series::Line;
+use charming::{component::Axis, element::Tooltip, Chart};
+
+use super::server_etl::{MetricData, CPU, RAM};
+use super::DURATION;
+
+fn calculate_moving_average(data: &[(f64, f64)], window_size: usize) -> Vec<(f64, f64)> {
+    let mut moving_average = Vec::new();
+    let mut sum = 0.0;
+
+    for (i, &(_, y)) in data.iter().enumerate() {
+        sum += y;
+        if i >= window_size {
+            sum -= data[i - window_size].1;
+            moving_average.push((data[i].0, sum / window_size as f64));
+        } else {
+            moving_average.push((data[i].0, sum / (i + 1) as f64));
+        }
+    }
+
+    moving_average
+}
+
+pub fn get_cpu_chart(cpu_data: Vec<Vec<(f64, CPU)>>) -> String {
+    let start_timestamp = cpu_data[0][0].0;
+    let data_set: Vec<Vec<CompositeValue>> = cpu_data
+        .clone()
+        .into_iter()
+        .map(|inner_vec| {
+            inner_vec
+                .into_iter()
+                .map(|(x, y)| {
+                    let active_time = y.user + y.system + y.nice + y.softirq + y.irq;
+                    let total_time = active_time + y.idle + y.iowait;
+                    let utilization = match total_time {
+                        t if t <= 0. => 0.,
+                        _ => active_time / total_time * 100.
+                    };
+                    CompositeValue::Array(vec![
+                    CompositeValue::Number(NumericValue::from(x - start_timestamp)),
+                    CompositeValue::Number(NumericValue::from(utilization)),
+                    ])
+                })
+                .collect()
+        })
+        .collect();
+
+    let mut chart = Chart::new()
+        .x_axis(
+            Axis::new()
+                .type_(AxisType::Value)
+                .boundary_gap(false)
+                .min(0)
+                .max(DURATION),
+        )
+        .y_axis(Axis::new().type_(AxisType::Value).boundary_gap(false))
+        .tooltip(Tooltip::new().trigger(Trigger::Axis));
+        
+
+    for (i, data) in data_set.iter().enumerate() {
+        chart = chart.series(
+                Line::new()
+                    .name(&format!("Core {}", i))
+                    .data(data.clone())
+                    .symbol(Symbol::None),
+        );
+    }
+    let options = serde_json::to_string(&chart).unwrap_or_default();
+    options
+}
+
+pub fn get_ram_chart(ram_data: Vec<(f64, RAM)>) -> String {
+    let start_timestamp = ram_data[0].0;
+    let data_set: Vec<CompositeValue> = ram_data
+        .clone()
+        .into_iter()
+        .map(|(x, y)| {
+            CompositeValue::Array(vec![
+                CompositeValue::Number(NumericValue::from(x - start_timestamp)),
+                CompositeValue::Number(NumericValue::from(y.total - y.available)),
+            ])
+        })
+        .collect();
+    let chart = Chart::new()
+        .x_axis(
+            Axis::new()
+                .type_(AxisType::Value)
+                .boundary_gap(false)
+                .min(0)
+                .max(DURATION),
+        )
+        .y_axis(Axis::new().type_(AxisType::Value).boundary_gap(false))
+        .tooltip(Tooltip::new().trigger(Trigger::Axis))
+        .series(
+            Line::new()
+                .name("Original")
+                .area_style(AreaStyle::new())
+                .data(data_set)
+                .symbol(Symbol::None),
+        );
+    let options = serde_json::to_string(&chart).unwrap_or_default();
+    options
+}
+
+pub fn get_metric_chart(metric_data: Vec<(f64, f64)>, max_y: f32) -> String {
+    let data_set: Vec<CompositeValue> = metric_data
+        .clone()
+        .into_iter()
+        .map(|(x, y)| {
+            CompositeValue::Array(vec![
+                CompositeValue::Number(NumericValue::from(x)),
+                CompositeValue::Number(NumericValue::from(y)),
+            ])
+        })
+        .collect();
+        let chart = Chart::new()
+        .x_axis(
+            Axis::new()
+                .type_(AxisType::Value)
+                .boundary_gap(false)
+                .min(0)
+                .max(DURATION),
+        )
+        .y_axis(Axis::new().type_(AxisType::Value).max(max_y).min(0).boundary_gap(false))
+        .tooltip(Tooltip::new().trigger(Trigger::Axis))
+        .series(
+            Line::new()
+                .name("Original")
+                .area_style(AreaStyle::new())
+                .data(data_set)
+                .symbol(Symbol::None),
+        );
+    let options = serde_json::to_string(&chart).unwrap_or_default();
+    options
+}
+
+pub struct MetricDataJson {
+    pub cpu: String,
+    pub ram: String,
+    pub send: String,
+    pub received: String,
+}
+
+pub fn get_instance_metric_data(server_version: u8) -> MetricDataJson {
+    let data = MetricData::new(&format!("v{}", server_version));
+
+    let send = get_metric_chart(data.send, 4.);
+    let received = get_metric_chart(data.received, 3000.);
+    let cpu = get_cpu_chart(data.cpu);
+    let ram = get_ram_chart(data.ram);
+    MetricDataJson {
+        cpu,
+        ram,
+        send,
+        received,
+    }
+}
